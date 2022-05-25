c0538aee3ac8ea939b97bb90dad66c8936fd0d42
vladimir <nlit@pm.me>
2022-05-25 06:57:21 +0300

    Favicon fix

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,6 +9,8 @@
 
     {{--  favicon creator  https://favicon.io --}}
     {{-- favicon generator https://realfavicongenerator.net/ --}}
+    <link rel="icon" href="favicon.ico">
+    <link rel="shortcut icon" href="favicon.ico">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
     <link rel="manifest" href="/favicon/site.webmanifest">
