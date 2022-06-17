03f6364b840327e25de944390f2ac936433eaee0
vladimir <nlit@pm.me>
2022-06-17 23:16:54 +0300

    Update projects

--- resources/js/components/pages/b-MyProjects.vue
+++ resources/js/components/pages/b-MyProjects.vue
@@ -1,21 +1,22 @@
 <template>
     <div>
         <div class="card-container mx-auto">
-            <b-project link="https://rosretail.com" job-type="teamwork" description-trans="rosretail" />
-            <b-project link="https://arendator.moscow" job-type="teamwork" description-trans="arendator"/>
+            <b-project title="WhiteWill" link="https://whitewill.london" job-type="teamwork" />
+            <b-project title="Marina Dubai" link="https://marinadubai.ae" job-type="teamwork" />
+            <b-project title="RosRetail" link="https://rosretail.com" job-type="teamwork" description-trans="rosretail" />
+            <b-project title="Arendator Moscow" link="https://arendator.moscow" job-type="teamwork"
+                       description-trans="arendator"/>
             <b-project title="Telegram Bots" job-type="solo_work" description-trans="telegram-bots" disable_preview="true" />
-            <b-project link="https://sofilena.ru" job-type="teamwork" description-trans="sofilena"/>
-            <b-project link="http://bonussofilena.ru" get_external_html="true" disable_preview="true"
+            <b-project title="MoscowCitySale" link="https://moscowcitysale.ru" job-type="teamwork" />
+            <b-project title="KupiteKvartiru" link="https://kupitekvartiru.com" job-type="teamwork" />
+            <b-project title="Hamovnyki" link="https://hamovnyki.ru" job-type="teamwork" />
+            <b-project title="KupiteLoft" link="https://kupiteloft.ru" job-type="teamwork" />
+            <b-project title="Barvikha" link="https://barvikha.moscow" job-type="teamwork" />
+            <b-project title="SofiLena" link="https://sofilena.ru" job-type="teamwork" description-trans="sofilena"/>
+            <b-project title="BonusSofilena" link="http://bonussofilena.ru" get_external_html="true" disable_preview="true"
                        job-type="solo_work" description-trans="bonussofilena"/>
-            <b-project link="https://whitewill.london" job-type="teamwork" />
-            <b-project link="https://marinadubai.ae" job-type="teamwork" />
-            <b-project link="https://kupitekvartiru.com" job-type="teamwork" />
-            <b-project link="https://moscowcitysale.ru" job-type="teamwork" />
-            <b-project link="https://kupiteloft.ru" job-type="teamwork" />
-            <b-project link="https://hamovnyki.ru" job-type="teamwork" />
-            <b-project link="https://silverbor.moscow" job-type="teamwork" />
-            <b-project link="https://barvikha.moscow" job-type="teamwork" />
-            <b-project link="https://osobnyaki.com" job-type="teamwork" />
+            <b-project title="Osobnyaki" link="https://osobnyaki.com" job-type="teamwork" />
+            <b-project title="Silver Bor" link="https://silverbor.moscow" job-type="teamwork" />
         </div>
         <b-footer blurred="1"/>
     </div>
