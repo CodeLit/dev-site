73e9deba61aa098f8f9b42b7aac4cd738d51d7b0
vladimir <nlit@pm.me>
2022-10-18 04:13:04 +0500

    proxy

--- public/internet/my-proxy.js
+++ public/internet/my-proxy.js
@@ -2,7 +2,7 @@ function FindProxyForURL(url, host) {
 
     // use proxy for specific domains
     if (shExpMatch(host, "music.youtube.com|2ip.ru"))
-        return "SOCKS5 51.38.191.30:7497";
+        return "SOCKS5 46.101.230.20:7497";
 
     // by default use no proxy
     return "DIRECT";
