bcaa641d58d902097b5edd467c0dd92a05d3a42b
vladimir <nlit@pm.me>
2023-04-04 04:07:17 +0400

    deployment test

--- docker/php-fpm/docker-entrypoint.sh
+++ docker/php-fpm/docker-entrypoint.sh
@@ -55,4 +55,10 @@ elif [ -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini ]; then
   rm -rf /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 fi
 
+composer update --working-dir=/var/www/laravel/current
+#php artisan config:cache
+#php artisan route:cache
+#php artisan view:cache
+#php artisan optimize
+
 exec "$@"
