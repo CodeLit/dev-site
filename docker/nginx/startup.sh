d59f9a6feae4eaae0728950f2a0bd35bee75337d
vladimir <nlit@pm.me>
2023-04-04 06:51:39 +0400

    hotfix

--- docker/nginx/startup.sh
+++ docker/nginx/startup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -f /etc/nginx/ssl/live/codelit.tk/fullchain.pem ]; then
+    mkdir -p /etc/nginx/ssl/live/codelit.tk
+    openssl genrsa -out /etc/nginx/ssl/live/codelit.tk/privkey.pem 2048
+    openssl req -new -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=codelit.tk" -key /etc/nginx/ssl/live/codelit.tk/privkey.pem -out /etc/nginx/ssl/live/codelit.tk/fullchain.pem
+fi
+
+
+
 # cron job to restart nginx every 6 hour
 (crontab -l ; echo "0 0 */4 * * nginx -s reload") | crontab -
 
