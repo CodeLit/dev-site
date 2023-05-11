1396d10edafdadad36eb585d7ed31616e0426a99
vladimir <nlit@pm.me>
2023-05-12 00:09:54 +0400

    Fix

--- useful/ubuntu.sh
+++
@@ -1,9 +0,0 @@
-# Default table setup
-iptables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
-/sbin/iptables-save
-
-# Reset tables
-iptables -t mangle -F
-iptables -F
-iptables -X
