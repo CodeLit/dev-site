75b7ac305324e6ff7f3ab432406337f2e6786289
CodeLit <nlit@pm.me>
2023-10-28 17:25:25 +0600

    Update #21

--- docker/mix/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-if [ "$MIX_WATCH_ON" == 'true' ]; then npm run watch; else npm run prod; fi
--- shell/build.sh
+++ shell/build.sh
@@ -3,8 +3,9 @@ script_dir=$(dirname "$(readlink -f "$0")")
 cd "$script_dir/.." || exit
 root_folder="$script_dir"/..
 cd "$root_folder" || exit
-if ! npm list -g yarn >/dev/null 2>&1; then # if there is no yarn npm package
-	npm install --global yarn
+
+if [ "$MIX_WATCH_ON" == "true" ]; then
+    yarn watch
+else
+    yarn production
 fi
-yarn install
-yarn production
