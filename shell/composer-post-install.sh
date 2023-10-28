a21b2db678cbdf971545d2fe71ace96cae929933
CodeLit <nlit@pm.me>
2023-10-28 19:31:15 +0600

    Update #21

---
+++ shell/composer-post-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+script_dir=$(dirname "$(readlink -f "$0")")
+cd "$script_dir/.." || exit
+root_folder="$script_dir"/..
+cd "$root_folder" || exit
+
+source .env
+if [ ! "$APP_KEY" ]; then
+    php artisan key:generate --ansi
+fi
