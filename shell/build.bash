a1fd62d9f7ff2bff19df7706ef2f0f27a23499ad
CodeLit <nlit@pm.me>
2023-10-28 22:40:25 +0600

    Fix #29

--- shell/build.bash
+++ shell/build.bash
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 script_dir=$(dirname "$(readlink -f "$0")")
 cd "$script_dir/.." || exit
 root_folder="$script_dir"/..
 cd "$root_folder" || exit
 
-if [ "$MIX_WATCH_ON" == "true" ]; then
+if [ "$MIX_WATCH_ON" = "true" ]; then
     yarn watch
 else
     yarn production
