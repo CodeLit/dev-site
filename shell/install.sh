fd0aaae34638d039227015abc1e5df8de7df3af2
Vladimir Gelunov <v.gelunov@it.ecommpay.com>
2023-11-03 04:01:26 +0600

    Deploy fixes

--- shell/install.sh
+++ shell/install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+export DEBIAN_FRONTEND=noninteractive
+sudo apt update
+
+# Check if the 'php' command is available
+if ! command -v php &> /dev/null; then
+    sudo add-apt-repository ppa:ondrej/php -y
+    sudo apt install -y php8.2-fpm php8.2-common
+fi
+
 if ! command -v composer &> /dev/null
 then
-    # Composer is not installed, so let's download and install it
     EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
     ACTUAL_SIGNATURE="$(php -r \"echo hash_file('sha384', 'composer-setup.php');\")"
@@ -21,13 +29,6 @@ fi
 
 if ! command -v node &> /dev/null
 then
-    # Node.js is not installed, so let's install it
-    if ! command -v curl &> /dev/null
-    then
-        >&2 echo 'ERROR: Curl is not installed. Please install curl.'
-        exit 1
-    fi
-
     curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
     sudo apt-get install -y nodejs
 fi
