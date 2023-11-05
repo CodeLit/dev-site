c984a31c9ba20aaf93114cc84d4f1eb60c6edb64
Vladimir Gelunov <v.gelunov@it.ecommpay.com>
2023-11-05 09:24:28 +0600

    Fix

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -4,10 +4,14 @@ cd "$script_dir/.." || exit
 root_folder="$script_dir"/..
 cd "$root_folder" || exit
 
+php artisan down
+
 composer install --no-dev --optimize-autoloader --no-interaction
 
 yarn install
 yarn production
+
+php artisan up
 
 # Reset user permissions after unsuccessful copy
 # First, update your .env file from .env.example. Then:
