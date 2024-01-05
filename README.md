df3a8602eb15a56d822a84f9a99cebeac18df5c0
CodeLit <nlit@pm.me>
2024-01-06 00:52:29 +0600

    #52 Favicon

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 This is a home project to demonstrate the techniques I have learned.
 
 [//]: # (<p align="center">)
-[//]: # (<img src="https://codelit.tk/favicon/android-chrome-512x512.png" width="400">)
+
+[//]: # (<img src="https://codelit.win/favicon/android-chrome-512x512.png" width="400">)
+
 [//]: # (</p>)
 
 <p>
@@ -13,6 +15,7 @@ This is a home project to demonstrate the techniques I have learned.
 Please enjoy your stay!
 
 ## Technologies used
+
 - `Laravel PHP framework`
 - `Vue.js`
 - `Tailwind.js`
@@ -23,6 +26,7 @@ Please enjoy your stay!
 ## Dev env launch
 
 ### Manual
+
 - Install PHP 8.2, Composer, Node LTS, yarn
 - Run `composer install`
 - Run `yarn install`
@@ -30,17 +34,21 @@ Please enjoy your stay!
 - Use php web server to run `public/index.php` file
 
 ### Using Docker container
+
 ```
 docker-compose up --build -d
 ```
 
 ## About Laravel
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and
+creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in
+many web projects, such as:
 
 - [Simple, fast routing engine](https://laravel.com/docs/routing).
 - [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
+- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache)
+  storage.
 - Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
 - Database agnostic [schema migrations](https://laravel.com/docs/migrations).
 - [Robust background job processing](https://laravel.com/docs/queues).
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,39 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-        <title>{{__('common.blog_name')}}</title>
+    <title>{{__('common.blog_name')}}</title>
 
-        {{-- Favicons--}}
-        @if(view()->exists('layout.favicon'))
-            @include('layout.favicon')
-        @endif
+    {{-- Favicons--}}
+    @if(view()->exists('layout.favicon'))
+        @include('layout.favicon')
+    @endif
 
+    {{-- Fonts--}}
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        {{-- Fonts--}}
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    {{-- Css --}}
+    <link href="/build/css/app.css" rel="stylesheet">
 
-        {{-- Css --}}
-        <link href="/build/css/app.css" rel="stylesheet">
+    {{-- Js --}}
+    <script src="/build/js/app.js" type="text/javascript"></script>
 
-        {{-- Js --}}
-        <script src="/build/js/app.js"></script>
+    {{-- Icons --}}
+    <script defer type="module" src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.esm.js"></script>
 
-        {{-- Icons --}}
-        <script defer type="module" src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.esm.js"></script>
-
-    </head>
-    <body>
-        <div id="app">
-            <h1>Vue.js Application</h1>
-            Build of this application is not loaded.
-            Please use yarn install and yarn run production to build the application.
-            <noscript>Please enable JavaScript on this page.</noscript>
-        </div>
-    </body>
+</head>
+<body>
+<div id="app">
+    <h1>Vue.js Application</h1>
+    Build of this application is not loaded.
+    Please use yarn install and yarn run production to build the application.
+    <noscript>Please enable JavaScript on this page.</noscript>
+</div>
+</body>
 </html>
