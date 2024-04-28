43b20e6f54e9949e4d1b255ac8bc502a55466b81
CodeLit <nlit@pm.me>
2024-04-29 01:22:50 +0500

    Update

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 up:
-	docker-compose up -d --build --remove-orphans
+	make build
+	make down
+	docker-compose -f docker-compose.dev.yml -f docker-compose.yml up --remove-orphans --force-recreate
 
 attach:
 	docker-compose exec -it dev-site bash
@@ -13,9 +15,13 @@ build:
 down:
 	docker-compose down
 
+prod:
+	if [ ! -f .env ]; then cp .env.example .env; fi
+	make down
+	docker-compose up -d --remove-orphans --force-recreate
+
 pull:
 	docker-compose pull
 
-prod:
-	if [ ! -f .env ]; then cp .env.example .env; fi
-	docker-compose up -d --remove-orphans
+push:
+	docker-compose push
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        strictPort: true,
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: false,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
+    optimizeDeps: {
+        include: [],
+    },
+})
