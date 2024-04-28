9027f8e76a273743c6e7fff7954cdbe3b33a5903
CodeLit <nlit@pm.me>
2024-04-28 15:11:45 +0500

    Fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ cd /app || exit
 
 # Copying node_modules and vendor to shared folder
 # From easy to copy to hard
-rsync -ahrvmP --remove-source-files /app/public /app/node_modules /app/vendor /shared
-rsync -ahrvmP --remove-source-files /app/resources/views/layout/* /shared/resources/views/layout
+rsync -ahrvP --remove-source-files /app/public /app/node_modules /app/vendor /shared
+rsync -ahrvP --remove-source-files /app/resources/views/layout/* /shared/resources/views/layout
 
 bash
