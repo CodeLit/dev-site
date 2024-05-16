2b54d088d116de4adee382df1f52529658906338
CodeLit <nlit@pm.me>
2024-05-16 21:40:25 +0500

    Fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,12 +14,13 @@ fi
 rsync -ahr /app/node_modules /app/vendor /shared &
 
 # If APP_ENV=local run Vite in dev mode
-if [ "$APP_ENV" = "local" ]; then
-    yarn # install dev dependencies
+if [ "$APP_ENV" = "production" ]; then
+    rm -f /shared/public/hot
+    # bash
+else
+    yarn # Install dev dependencies
     rm -rf /shared/public/build
     bun dev
-else
-    rm -f /shared/public/hot
 fi
 
-bash
+
