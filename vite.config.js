7a78d921182cda9be5186b0195094d996878c8e0
CodeLit <nlit@pm.me>
2024-11-11 10:40:10 +0100

    #68: WIP

--- vite.config.js
+++ vite.config.js
@@ -50,4 +50,11 @@ export default defineConfig({
     optimizeDeps: {
         include: [],
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern',
+            },
+        },
+    },
 })
