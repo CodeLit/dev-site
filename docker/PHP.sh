549ad77d89ecd54cb2abe1c3aceb4b179731b3e1
CodeLit <nlit@pm.me>
2024-11-13 00:27:41 +0100

    #91: WIP
    Signed-off-by: CodeLit <nlit@pm.me>

--- docker/PHP.sh
+++ docker/PHP.sh
@@ -2,6 +2,10 @@
 
 cd /app || exit
 
+# Fix rights on production
+chown -R www-data:www-data /app/storage /app/bootstrap/cache
+chmod -R 775 /app/storage /app/bootstrap/cache
+
 source .env
 
 php artisan storage:link -q
@@ -12,10 +16,6 @@ dockerize -wait tcp://mysql:3306 -timeout 30s -wait-retry-interval 500ms
 php artisan migrate --no-interaction --force &
 
 echo "Running in $APP_ENV mode"
-
-# Fix rights on production
-chown -R www-data:www-data /app/storage /app/bootstrap/cache
-chmod -R 775 /app/storage /app/bootstrap/cache
 
 if [ "$APP_ENV" != "production" ]; then
     php artisan serve --host=0.0.0.0 --port=80
