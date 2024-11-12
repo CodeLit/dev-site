3ead9be52a8e85f549a9f75f9c9ef7d607664115
CodeLit <nlit@pm.me>
2024-11-13 00:25:00 +0100

    #91: WIP
    Signed-off-by: CodeLit <nlit@pm.me>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,17 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <title inertia>{{__('common.blog_name')}}</title>
+    <title inertia>{{ __('common.blog_name') }}</title>
     <meta charset="utf-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=1, initial-scale=1, maximum-scale=5, minimum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="{{__('common.meta_description')}}.">
+    <meta name="description" content="{{ __('common.meta_description') }}.">
 
-
-    {{-- Fonts--}}
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    {{-- Fonts --}}
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Nunito:200,600&display=swap" as="style"
+          onload="this.onload=null;this.rel='stylesheet'">
 
     {{-- Ziggy JS routes --}}
     @routes
@@ -21,12 +21,7 @@
     @vite('resources/js/app.js')
 
     @inertiaHead
-</head>
-<body>
-    <noscript>Please enable JavaScript on this page.</noscript>
-    @inertia('app')
-    @yield('content')
-    @yield('scripts')
+
     <style>
         html:not([data-theme]) {
             background-color: #D9F6FF;
@@ -38,5 +33,11 @@
             }
         }
     </style>
+</head>
+<body>
+    <noscript>Please enable JavaScript on this page.</noscript>
+    @inertia('app')
+    @yield('content')
+    @yield('scripts')
 </body>
 </html>
